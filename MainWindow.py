291e929de1d81db738c1c87964992ab903db3582
Harry de Winton <harrydewinton@gmail.com>
2020-07-24 12:52:54 +0200

    linked with porosity.py and imported and used functions

--- MainWindow.py
+++ MainWindow.py
@@ -8,6 +8,8 @@ import numpy as np
 import cv2
 import qimage2ndarray
 
+from Porosity import findSectionMask
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,14 +24,20 @@ class MainWindow(QMainWindow):
         self.button_2.clicked.connect(self.binarise)
 
     def clicked(self):
-        self.data = cv2.imread('Camera_mount.png')[..., ::-1]
+        self.data = cv2.imread('example.jpeg')[..., ::-1]
         self.qimage = qimage2ndarray.array2qimage(self.data)
         self.setImage()
 
     def binarise(self):
-        self.grey = cv2.cvtColor(self.data, cv2.COLOR_RGB2GRAY)
-        _, self.binImg = cv2.threshold(self.grey, 150, 255, cv2.THRESH_BINARY)
-        self.qimage = qimage2ndarray.array2qimage(self.binImg)
+        #self.grey = cv2.cvtColor(self.data, cv2.COLOR_RGB2GRAY)
+        #_, self.binImg = cv2.threshold(self.grey, 150, 255, cv2.THRESH_BINARY)
+
+        gray = cv2.cvtColor(self.data, cv2.COLOR_RGB2GRAY)
+        binImg = cv2.threshold(gray, int(100), 255, cv2.THRESH_BINARY)[1]
+        masks = findSectionMask(binImg, area='hatch')
+        res = np.multiply(gray, masks[0])
+
+        self.qimage = qimage2ndarray.array2qimage(res)
         self.setImage()
 
     def resizeEvent(self, event):
